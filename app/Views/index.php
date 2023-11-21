2a29f7e78943cae0c32444dde2d91f311e782af3
dzskiii <dzakiasari86@gmail.com>
2023-11-21 15:59:34 +0700

    first commit

---
+++ app/Views/index.php
@@ -0,0 +1,326 @@
+<?php 
+
+$arrEvents = array("PAA",
+"IT Competition",
+"Hi-Technology",
+"HMTI Family");
+
+$arrLinks = array("https://www.instagram.com/reel/CkU5tJgOYVd/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA",
+"https://www.instagram.com/p/CnybOg6MJ2_/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA",
+"https://www.instagram.com/reel/CqFMBdRtmq5/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA",
+"https://www.instagram.com/reel/Clnx61WOH3q/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA");
+
+$lenArr = count($arrEvents);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <title>OCP 2023</title>
+    <link rel="icon" type="image/png" href="<?= base_url('logo.ico') ?>">
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?= base_url('vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="<?= base_url('assets/css/fontawesome.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/templatemo-tale-seo-agency.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/owl.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/animate.css') ?>">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 582 Tale SEO Agency
+
+https://templatemo.com/tm-582-tale-seo-agency
+
+-->
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Pre-Header Area Start ***** -->
+  <div class="pre-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-sm-9">
+          <div class="left-info">
+            <ul>
+              <li><a href="https://wa.me/+6285876215725" target="blank"><i class="fa fa-phone"></i>+6285876215725</a></li>
+              <li><a href="mailto:udinus.hmti@gmail.com" target="blank"><i class="fa fa-envelope"></i>udinus.hmti@gmail.com</a></li>
+              <li><a href="https://goo.gl/maps/kfxvX95MoyzqPHcy6" target="blank"><i class="fa fa-map-marker"></i>Universitas Dian Nuswantoro</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-3">
+          <div class="social-icons">
+            <ul>
+              <li><a href="https://facebook.com/hmti.udinus/" target="blank"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="https://twitter.com/hmtiudinus" target="blank"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="https://www.instagram.com/hmtiudinus/" target="blank"><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Pre-Header Area End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="#" class="logo">
+                        <img src="<?= base_url('assets/images/logoocp.png') ?>" alt="" style="max-width: 110px;">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                      <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                      <li class="scroll-to-section"><a href="#bidang">Bidang</a></li>
+                      <li class="scroll-to-section"><a href="#kegiatan">Kegiatan</a></li>
+                      <li class="scroll-to-section"><a href="#benefit">Benefit</a></li>
+                      <li class="scroll-to-section"><a href="#daftar">Daftar</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="main-banner" id="top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10">
+          <div class="caption header-text">
+            <h6>Orientasi Calon Pengurus 2023</h6>
+            <div class="line-dec"></div>
+            <h4>Explore <em>Your Potential </em>To Improve <span>Your Ability</span></h4>
+            <p>Orientasi Calon Pengurus merupakan suatu kegiatan bagi mahasiswa Universitas Dian Nuswantoro, khususnya mahasiswa Teknik Informatika, dimana mereka dapat bergabung bersama kami Himpunan Mahasiswa Teknik Informatika melalui serangkaian tahapan yang bermakna, bermanfaat, dan menyenangkan. Tentunya akan ada banyak manfaat yang didapat dengan bergabung bersama kami, seperti Softskill dan Hardskill yang akan terus diasah hingga mencapai potensi maksimal masing - masing. Pada tahun 2023 ini, Orientasi Calon Pengurus mengusung tema "Explore Your Potential To Improve Your Ability"</p>
+            <div class="main-button scroll-to-section"><a href="#benefit">Benefit</a></div>
+            <span></span>
+            <div class="second-button"><a href="#kegiatan">Kegiatan</a></div>
+            <span></span>
+            <div class="second-button"><a href="#daftar">Gabung Sekarang!</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="services section" id="bidang">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 offset-lg-6">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="section-heading">
+                <h2>3 <em>Bidang</em> Himpunan Mahasiswa Teknik Informatika</h2>
+                  <div class="line-dec"></div>
+              </div>
+            </div>
+            <div class="col-lg-12 col-sm-6">
+              <div class="service-item">
+                <div class="icon">
+                  <img src="<?= base_url('assets/images/logoiptek.png') ?>" alt="Bidang IPTEK" class="templatemo-feature">
+                </div>
+                <h4>Ilmu Pengetahuan dan Teknologi</h4>
+                <p>Apa sih Bidang IPTEK itu? Bidang IPTEK berperan aktif dalam usaha peningkatan penalaran dan pengembangan keilmuan pengurus dan mahasiswa teknik informatika.</p>
+              </div>
+            </div>
+            <div class="col-lg-12 col-sm-6">
+              <div class="service-item">
+                <div class="icon">
+                  <img src="<?= base_url('assets/images/logolitbang.png') ?>" alt="Bidang LITBANG" class="templatemo-feature">
+                </div>
+                <h4>Penelitian dan Pengembangan</h4>
+                <p>Nah kalau Bidang LITBANG apa sih? Bidang LITBANG ini berperan dalam penelitian, pembinaan, pengawasan, serta peningkatan kualitas sumber daya manusia organisasi dan mahasiswa mahasiswa teknik informatika.</p>
+              </div>
+            </div>
+            <div class="col-lg-12 col-sm-6">
+              <div class="service-item">
+                <div class="icon">
+                  <img src="<?= base_url('assets/images/logoinfokom.png') ?>" alt="Bidang INFOKOM" class="templatemo-feature">
+                </div>
+                <h4>Informasi dan Komunikasi</h4>
+                <p>Dan yang terakhir ada Bidang INFOKOM. Bidang ini berperan sebagai narahubung dan membangun hubungan baik dengan pihak luar HM-TI dan juga bergerak dalam bidang kreatif HM-TI.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="projects section" id="kegiatan">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="section-heading">
+            <h2>Discover Our <em>Events</em> &amp; <span>Journey</span></h2>
+            <div class="line-dec"></div>
+            <p>Berikut adalah beberapa dokumentasi kegiatan kami!</p>
+          </div>
+        </div>
+      </div> 
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="owl-features owl-carousel">
+            <!-- looping item -->
+            <?php for($i = 1 ; $i <= $lenArr ; $i++): ?>
+            <div class="item">
+              <img src="<?= base_url() ?>/assets/images/img-<?= $i ?>.jpg" alt="">
+              <div class="down-content">
+                <h4><?= $arrEvents[$i-1]?></h4>
+                <a href="<?=$arrLinks[$i-1]?>" target="blank"><i class="fa fa-link"></i></a>
+              </div>
+            </div>
+            <?php endfor ?>
+            <!-- looping item -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="infos section" id="benefit">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="main-content">
+            <div class="row">
+              <div class="col-lg-6">
+                <div class="left-image">
+                  <img src="<?= base_url('assets/images/barangbagus.png') ?>" alt="">
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="section-heading">
+                  <h2><span>Benefits</span></h2>
+                  <div class="line-dec"></div>
+                </div>
+                <div class="skills">
+                  <div class="skill-slide">
+                    <h6>Hardskill</h6>
+                    <!-- <span>90%</span> -->
+                    <p>Dengan menjadi bagian dari Pengurus HMTI hardskill kalian akan di tingkatin nih, hard skill tersebut antara lain : Desain Web, desain grafis, Editing video, dan masih banyak lagi. Tentunya kalian tidak akan belajar sendiri, kalian bisa saling share pengetahuan kalian dengan teman dan juga kakak tingkat.</p>
+                  </div>
+                  <div class="skill-slide">
+                    <h6>Softskill</h6>
+                    <!-- <span>80%</span> -->
+                    <p>Selain hard skill, kalian juga akan mendapatkan pembekalan untuk mengembangkan soft skill kalian antara lain kepercayaan diri, public speaking, manajemen waktu, bekerja sama dalam tim, dan tentunya masih banyak lagi pengalaman pengalaman yang dapat kalian gunakan untuk mengembangkan soft skill teman teman.</p>
+                  </div>
+                  <div class="skill-slide">
+                    <h6>Relasi</h6>
+                    <!-- <span>95%</span> -->
+                    <p>Dengan menjadi bagian dari Pengurus HMTI kalian juga akan mendapat banyak teman teman baru, dengan begini kalian akan mendapatkan relasi yang semakin luas. Selain relasi dengan teman teman satu angkatan kalian juga dapat melakukan proyek dari dosen.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="contact-us section" id="daftar">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="contact-us-content">
+            <div class="row">
+              <div class="col-lg-6">
+                <!-- INI AUTO SLIDE -->
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" >
+                  <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="3000">
+                      <img src="<?= base_url('assets/images/Syarat1.png') ?>" alt="..." style="border:0; border-radius: 23px;">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="3000">
+                      <img src="<?= base_url('assets/images/Pemberkasan1.png') ?>" alt="..." style="border:0; border-radius: 23px;">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="3000">
+                      <img src="<?= base_url('assets/images/Timeline.png') ?>" alt="..." style="border:0; border-radius: 23px;">
+                    </div>
+                  </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
+                </div>
+              </div>
+              <div class="col-lg-6">
+                <div class="row justify-content-center" id="contact-form">
+                  <div class="col-lg-12">
+                    <div class="section-heading">
+                      <h2><em>Gabung Sekarang!</em></h2>
+                      <p>Bersama-sama, kita dapat menciptakan momentum yang kuat, membangun hubungan yang bermakna, dan merangkul keragaman untuk mencapai tujuan bersama. Jadilah bagian dari pergerakan ini, dan jadilah penggerak yang membawa perubahan dan inspirasi bagi sesama mahasiswa!</p>
+                      <a href="https://forms.gle/2i28VCNCgzxzkFoe8" target="blank">
+                        <img src="<?= base_url('assets/images/tomboll.png') ?>" alt="Daftar Sekarang!">
+                      </a>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="col-lg-12">
+        <p>Copyright © 2023 <a href="https://hmtiudinus.org/">HM-TI UDINUS</a>. All rights reserved.
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="<?= base_url('vendor/jquery/jquery.min.js') ?>"></script>
+  <script src="<?= base_url('vendor/bootstrap/js/bootstrap.min.js') ?>"></script>
+
+  <script src="<?= base_url('assets/js/isotope.min.js') ?>"></script>
+  <script src="<?= base_url('assets/js/owl-carousel.js') ?>"></script>
+  <script src="<?= base_url('assets/js/tabs.js') ?>"></script>
+  <script src="<?= base_url('assets/js/popup.js') ?>"></script>
+  <script src="<?= base_url('assets/js/custom.js') ?>"></script>
+
+
+  </body>
+
+</html>
